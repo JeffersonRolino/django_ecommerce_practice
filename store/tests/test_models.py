4d6d72ec908fe565762d30195a0d33d05bb4d511
jeffersonrolino <jefferson.rolino@gmail.com>
2023-10-16 08:06:38 -0300

    Testando inserção, tipos e campos do modelo Product

--- store/tests/test_models.py
+++ store/tests/test_models.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.models import User
 from django.test import TestCase
 
 from store.models import Category, Product
@@ -21,3 +22,19 @@ class TestCategoriesModel(TestCase):
         """
         data = self.data1
         self.assertEqual(str(data), 'django')
+
+
+class TestProductsModel(TestCase):
+    def setUp(self):
+        Category.objects.create(name='django', slug='django')
+        User.objects.create(username='admin')
+        self.data1 = Product.objects.create(category_id=1, title='django beginners',
+                                            created_by_id=1, slug='django-beginners', price='20.00', image='django')
+
+    def test_product_model_entry(self):
+        """
+        Test Product model data insertion/types/field atributes
+        """
+        data = self.data1
+        self.assertTrue(isinstance(data, Product))
+        self.assertEqual(str(data), 'django beginners')
