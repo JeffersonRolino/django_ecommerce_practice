376790e6ca13b7d676ad191a1f47b796186bc37e
jeffersonrolino <jefferson.rolino@gmail.com>
2023-10-24 11:17:13 -0300

    Adding the Quantity to the Session Data

--- basket/basket.py
+++ basket/basket.py
@@ -13,13 +13,20 @@ class Basket():
             basket = self.session['skey'] = {}
         self.basket = basket
 
-    def add(self, product):
+    def add(self, product, product_qty):
         """
         Adding and updating the users basket session data
         """
         product_id = str(product.id)
 
         if product_id not in self.basket:
-            self.basket[product_id] = {'price': str(product.price)}
+            self.basket[product_id] = {'price': str(product.price), 'qty': int(product_qty)}
 
         self.session.modified = True
+
+    def __len__(self):
+        """
+        Get the basket data and count the qty of items
+        """
+
+        return sum(item['qty'] for item in self.basket.values())
